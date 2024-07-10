be64ebf5522d9da46f97757d983e61c2c8461461
Dmitry Andreyev <Krygliy.ya@gmail.com>
2024-07-10 21:39:05 +0700

    index types

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import express from 'express';
+import expressModule from 'express';
 import bodyParser from 'body-parser';
-import registerRouter from './routes/registerRouter.js';
-import testRouter from './routes/testRouter.js';
 import cors from 'cors';
 
-const app = express();
+import testRouter from './routes/testRouter';
+import registerRouter from './routes/registerRouter';
+
+
+const app = expressModule();
+
 
 app.use(bodyParser.json());
 
@@ -18,6 +21,10 @@ app.use('/register', registerRouter)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
 
+
+function express() {
+	throw new Error('Function not implemented.');
+}
 // Добавить типизацию
 // переписать с bcypt на jsonwebtokens
 // реализовать аутентификацию пользователя
